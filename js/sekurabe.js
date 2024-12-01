a323e2fd09cc285ebdafd5402242fac95120b6fe
m15obayasi <m15obayasi@gmail.com>
2024-12-02 00:12:14 +0900

    今日はもう寝ます。

--- js/sekurabe.js
+++ js/sekurabe.js
@@ -1,29 +1,44 @@
-$(".contents-2, .answer").hide();
+$(".pokeTable, .answer, .checkTable").hide();
 
-$(".contents-1").on("click", function () {
-    $(".contents-1").hide();
-    $(".contents-2, .answer").show();
-    getPokemon();
+$(".contents").on("click", function () {
+    $(".pokeTable, .checkTable").show();
+    $(".contents").hide();
+    $(".answer").fadeIn(1000);
+    getPokemonA();
 });
 
-
-function getPokemon(){
-    const random = Math.floor(Math.random() * 151) + 1;
-    console.log(random);
-    const url = `https://pokeapi.co/api/v2/pokemon/${random}`;
-    console.log(url);
-    const res = fetch(url);
-    console.log(res);
-    const pokemon = res.json();
-    console.log(pokemon);
-    const randomId = random.sprites.front_default;
-    console.log(randomId);
-    createPokemon(pokemon);
+// image-1用の関数
+async function getPokemonA(){
+    const randomA = Math.floor(Math.random() * 99) + 1;
+    const urlA = `https://pokeapi.co/api/v2/pokemon/${randomA}`;
+    const resA = await fetch(urlA);
+    const pokemonA = await resA.json();
+    console.log(pokemonA);
+    createPokemonA(pokemonA);
+    getPokemonB();
 };
 
-
-function createPokemon(pokemon,) {
-    $(".image-1").html(`<img src="${randomId}">`);
-    $(".image-2").html(`<img src="${pokemon.sprites.front_default}">`);
-    $(".image-3").html(`<img src="${pokemon.sprites.front_default}">`);
+// image-2用の関数
+async function getPokemonB(){
+    const randomB = Math.floor(Math.random() * 199) + 100;
+    const urlB = `https://pokeapi.co/api/v2/pokemon/${randomB}`;
+    const resB = await fetch(urlB);
+    const pokemonB = await resB.json();
+    console.log(pokemonB);
+    createPokemonB(pokemonB);
 };
+
+// pokemonAをimage-1に掲載
+function createPokemonA(pokemonA) {
+    $(".image-1").html(`<img src="${pokemonA.sprites.front_default}">`);
+};
+
+// pokemonBをimage-2に掲載
+function createPokemonB(pokemonB) {
+    $(".image-2").html(`<img src="${pokemonB.sprites.front_default}">`);
+    $(".image-3").html(`<img src="./img/dedo.png"></img>`);
+};
+
+$(".check-1").on("click", function () {
+    $(".check-1").html("高");
+});
