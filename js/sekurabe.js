62f74f18740fc2bc9bf3037ec28af54da14151e6
m15obayasi <m15obayasi@gmail.com>
2024-12-01 21:40:35 +0900

    好きな色味です。

--- js/sekurabe.js
+++ js/sekurabe.js
@@ -1,23 +1,23 @@
-$(".contents-2").hide();
+$(".contents-2, .answer").hide();
 
 $(".contents-1").on("click", function () {
     $(".contents-1").hide();
-    $(".contents-2").show();
+    $(".contents-2, .answer").show();
     getPokemon();
 });
 
 
 function getPokemon(){
     const random = Math.floor(Math.random() * 151) + 1;
-        console.log(random);
+    console.log(random);
     const url = `https://pokeapi.co/api/v2/pokemon/${random}`;
-        console.log(url);
+    console.log(url);
     const res = fetch(url);
-        console.log(res);
+    console.log(res);
     const pokemon = res.json();
-        console.log(pokemon);
+    console.log(pokemon);
     const randomId = random.sprites.front_default;
-        console.log(randomId);
+    console.log(randomId);
     createPokemon(pokemon);
 };
 
