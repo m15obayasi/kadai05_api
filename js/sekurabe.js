82100b7e88a496d8b532b535cdf41b8da14a94a7
m15obayasi <m15obayasi@gmail.com>
2024-12-01 20:47:03 +0900

    完全に冬ですね。

---
+++ js/sekurabe.js
@@ -0,0 +1,29 @@
+$(".contents-2").hide();
+
+$(".contents-1").on("click", function () {
+    $(".contents-1").hide();
+    $(".contents-2").show();
+    getPokemon();
+});
+
+
+function getPokemon(){
+    const random = Math.floor(Math.random() * 151) + 1;
+        console.log(random);
+    const url = `https://pokeapi.co/api/v2/pokemon/${random}`;
+        console.log(url);
+    const res = fetch(url);
+        console.log(res);
+    const pokemon = res.json();
+        console.log(pokemon);
+    const randomId = random.sprites.front_default;
+        console.log(randomId);
+    createPokemon(pokemon);
+};
+
+
+function createPokemon(pokemon,) {
+    $(".image-1").html(`<img src="${randomId}">`);
+    $(".image-2").html(`<img src="${pokemon.sprites.front_default}">`);
+    $(".image-3").html(`<img src="${pokemon.sprites.front_default}">`);
+};
