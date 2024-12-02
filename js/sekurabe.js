b8336d95220649b3a7c7c209198a298fc98c1226
m15obayasi <m15obayasi@gmail.com>
2024-12-02 20:58:49 +0900

    暖房つけます。

--- js/sekurabe.js
+++ js/sekurabe.js
@@ -1,5 +1,15 @@
 $(".pokeTable, .answer, .checkTable").hide();
 
+let clickCount = 0;
+const states = ["高", "中", "低"];
+const dydoHeight = 18.3;
+
+// ここにポケモンの身長を格納
+let numbers = [
+    dydoHeight
+];
+
+
 $(".contents").on("click", function () {
     $(".pokeTable, .checkTable").show();
     $(".contents").hide();
@@ -28,17 +38,47 @@ async function getPokemonB(){
     createPokemonB(pokemonB);
 };
 
-// pokemonAをimage-1に掲載
+// pokemonAをimage-1に掲載、身長を格納
 function createPokemonA(pokemonA) {
     $(".image-1").html(`<img src="${pokemonA.sprites.front_default}">`);
+    let heightA = pokemonA.height;
+    numbers.push(heightA);
 };
 
-// pokemonBをimage-2に掲載
+// pokemonBをimage-2に掲載、身長を格納
 function createPokemonB(pokemonB) {
     $(".image-2").html(`<img src="${pokemonB.sprites.front_default}">`);
     $(".image-3").html(`<img src="./img/dedo.png"></img>`);
+    let heightB = pokemonB.height;
+    numbers.push(heightB);
+    console.log(numbers);
+    // 高い順に並び替え
+    numbers.sort(function(a, b) {
+        return b - a;
+    });
+    console.log(numbers);
 };
 
+// 高 → 中 → 低
 $(".check-1").on("click", function () {
-    $(".check-1").html("高");
+    clickCount++;
+    const index = clickCount % states.length;
+    $(".check-1").html(states[index]);
+});
+
+$(".check-2").on("click", function () {
+    clickCount++;
+    const index = clickCount % states.length;
+    $(".check-2").html(states[index]);
+});
+
+$(".check-3").on("click", function () {
+    clickCount++;
+    const index = clickCount % states.length;
+    $(".check-3").html(states[index]);
+});
+
+// 回答アクション
+$(".answer").on("click", function () {
+    
 });
